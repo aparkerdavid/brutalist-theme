215a3f6382982086261f511b9d5d400e3f44a50c
Parker Rueve <parkerrueve@Parkers-MacBook-Pro.local>
2020-10-22 03:24:02 -0400

    sane hl-line

--- brutalist-build.el
+++ brutalist-build.el
@@ -78,7 +78,7 @@
    `(fringe ((t (:background ,bg :foreground ,fg))))
    `(highlight ((t nil)))
    `(lazy-highlight ((t (:background ,bg-highlight-3 :slant italic))))
-   `(hl-line ((t (:inverse-video t))))
+   `(hl-line ((t (:background "#eeeee8"))))
    `(ido-first-match ((t (:foreground ,fg))))
    `(ido-only-match ((t (:foreground ,fg))))
    `(ido-subdir ((t (:foreground ,fg))))
