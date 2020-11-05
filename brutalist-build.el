e3647afcd7a3735205332f78199b1951fb4d3156
Parker Rueve <parkerrueve@Parkers-MacBook-Pro.local>
2020-11-05 11:25:13 -0500

    Fixed bug with lsp-faces background

--- brutalist-build.el
+++ brutalist-build.el
@@ -314,8 +314,8 @@
    ;; auto-dim-other-buffers
    `(auto-dim-other-buffers-face ((t (:background ,bg-light))))
    ;; lsp
-   `(lsp-modeline-code-actions-preferred-face ((t (:foreground ,fg :background bg-highlight-3))))
-   `(lsp-ui-sideline-code-action   ((t (:foreground ,fg :background bg-highlight-3))))
+   `(lsp-modeline-code-actions-preferred-face ((t (:foreground ,fg :background ,bg-highlight-3))))
+   `(lsp-ui-sideline-code-action   ((t (:foreground ,fg :background ,bg-highlight-3))))
    ))
 
 
